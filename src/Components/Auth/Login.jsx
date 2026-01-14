0ec3901737489a103c7a1c1fd729b4526edf8f56
Nobodip Debnath <nobodipdebnath45@gmail.com>
2026-01-14 21:36:09 +0600

    add file

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,47 +1,92 @@
-import React from 'react';
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { MdOutlineMail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
-import { Link } from 'react-router';
+import { Link } from "react-router";
+import { FaEyeSlash } from "react-icons/fa6";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div className='h-screen inter relative'>
-            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-10 py-14 border border-primary1 rounded-[10px] flex flex-col items-center justify-center'>
-                <h1 className='inter text-4xl font-semibold text-[#FAFAFD]'>Login</h1>
-                <p className='text-base text-[#FAFAFD] mt-3'>Don’t have an account? <Link  to='/login/register' className='text-sm text-[#999999] underline cursor-pointer'>Create account</Link></p>
-                <button class="px-[160px] py-3 rounded-[10px] font-semibold cursor-pointer mt-10 bg-[#C5BB95] text-[#2D2D2D] flex items-center gap-2 w-full border-[#e5e5e5]">
-                    <FcGoogle></FcGoogle>
-                    <span>Login with Google</span>
-                </button>
-                <div className='flex items-center gap-3 mt-8'>
-                    <div className='h-[1px] w-[210px] bg-[#C5BB95]'></div>
-                    <span className='text-[#FAFAFD]'>or</span>
-                    <div className='h-[1px] w-[210px] bg-[#C5BB95]'></div>
-                </div>
-                <div className='mt-10 relative w-full'>
-                    <input type="text" className='py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]' placeholder='Email' />
-                    <div className='absolute left-2 top-5 '>
-                        <MdOutlineMail className='text-white text-2xl '></MdOutlineMail>
-                    </div>
-                </div>
-                <div className='mt-10 relative w-full'>
-                    <input type="text" className='py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]' placeholder='Email' />
-                    <div className='absolute left-2 top-5.5 '>
-                        <FaLock className='text-white text-xl '></FaLock>
-                    </div>
-                    <div className='absolute right-5 top-6 cursor-pointer '>
-                        <FaEye className='text-white text-xl '></FaEye>
-                    </div>
-                </div>
-                <button class="w-full py-3 rounded-[10px] font-semibold cursor-pointer mt-10 bg-[#C5BB95] text-[#2D2D2D]  border-[#e5e5e5]">
-                    login
-                </button>
-                <p className='text-sm text-[#999999] mt-10'>Privacy Policy and Terms of Service</p>
-            </div>
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="h-screen inter relative">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-3 lg:px-10 py-8 lg:py-14 md:w-auto w-[90%] border border-primary1 rounded-[10px] flex flex-col items-center justify-center">
+        <h1 className="inter text-4xl font-semibold text-[#FAFAFD]">Login</h1>
+        <p className="text-base text-[#FAFAFD] mt-3">
+          Don’t have an account?{" "}
+          <Link
+            to="/login/register"
+            className="text-sm text-[#999999] underline cursor-pointer"
+          >
+            Create account
+          </Link>
+        </p>
+        <button class="lg:px-[160px] py-3 rounded-[10px] font-semibold cursor-pointer mt-5 lg:mt-10 bg-[#C5BB95] text-[#2D2D2D] flex items-center justify-center gap-2 w-full border-[#e5e5e5]">
+          <FcGoogle></FcGoogle>
+          <span>Login with Google</span>
+        </button>
+        <div className="flex items-center gap-3 mt-4 lg:mt-8">
+          <div className="h-[1px] w-25 lg:w-[210px] bg-[#C5BB95]"></div>
+          <span className="text-[#FAFAFD]">or</span>
+          <div className="h-[1px] w-25 lg:w-[210px] bg-[#C5BB95]"></div>
         </div>
-    );
+        <div className="lg:mt-10 mt-4 relative w-full">
+          <input
+            type="email"
+            {...register("email", {required: true})}
+            className="md:py-5 py-4 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]"
+            placeholder="Email"
+          />
+          {
+            errors.email && <p className="mt-1 text-red-500 text-sm">Email is required</p>
+          }
+          <div className="absolute left-2 top-4 md:top-5 ">
+            <MdOutlineMail className="text-white text-2xl "></MdOutlineMail>
+          </div>
+        </div>
+        <div className="mt-10 relative w-full">
+          <input
+            type={showPassword ? "text" : "password"}
+            {...register("password", {required: true})}
+            className="md:py-5 py-4 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]"
+            placeholder="Password"
+          />
+          {
+            errors.password && <p className="mt-1 text-red-500 text-sm">Password is required</p>
+          }
+          <div
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-5 top-5 md:top-5 cursor-pointer "
+          >
+            {showPassword ? (
+              <FaEye className="text-white text-xl" />
+            ) : (
+              <FaEyeSlash className="text-white text-xl" />
+            )}
+          </div>
+          <div className="absolute left-2 md:top-5.5 top-5 ">
+            <FaLock className="text-white text-xl "></FaLock>
+          </div>
+        </div>
+        <button onClick={handleSubmit(onSubmit)} class="w-full py-3 rounded-[10px] font-semibold cursor-pointer mt-10 bg-[#C5BB95] text-[#2D2D2D]  border-[#e5e5e5]">
+          login
+        </button>
+        <p className="text-sm text-[#999999] mt-10">
+          Privacy Policy and Terms of Service
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,55 +1,116 @@
-import React from 'react';
-import { FaEye, FaLock } from 'react-icons/fa';
-import { FcGoogle } from 'react-icons/fc';
-import { MdOutlineMail } from 'react-icons/md';
-import { Link } from 'react-router';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaLock } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { MdOutlineMail } from "react-icons/md";
+import { Link } from "react-router";
 
 const Register = () => {
-    return (
-        <div className='h-screen inter relative'>
-                    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-10 py-14 border border-primary1 rounded-[10px] flex flex-col items-center justify-center'>
-                        <h1 className='inter text-4xl font-semibold text-[#FAFAFD]'>Sign Up</h1>
-                        <p className='text-base text-[#FAFAFD] mt-3'>You have an already  account? <Link to='/login' className='text-sm text-[#999999] underline cursor-pointer'>Sign up</Link></p>
-                        <button class="px-[160px] py-3 rounded-[10px] font-semibold cursor-pointer mt-10 bg-[#C5BB95] text-[#2D2D2D] flex items-center gap-2 w-full border-[#e5e5e5]">
-                            <FcGoogle></FcGoogle>
-                            <span>Login with Google</span>
-                        </button>
-                        <div className='flex items-center gap-3 mt-8'>
-                            <div className='h-[1px] w-[210px] bg-[#C5BB95]'></div>
-                            <span className='text-[#FAFAFD]'>or</span>
-                            <div className='h-[1px] w-[210px] bg-[#C5BB95]'></div>
-                        </div>
-                        <div className='mt-10 relative w-full'>
-                            <input type="text" className='py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]' placeholder='Email' />
-                            <div className='absolute left-2 top-5 '>
-                                <MdOutlineMail className='text-white text-2xl '></MdOutlineMail>
-                            </div>
-                        </div>
-                        <div className='mt-10 relative w-full'>
-                            <input type="password" className='py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]' placeholder='Password' />
-                            <div className='absolute left-2 top-5.5 '>
-                                <FaLock className='text-white text-xl '></FaLock>
-                            </div>
-                            <div className='absolute right-5 top-6 cursor-pointer '>
-                                <FaEye className='text-white text-xl '></FaEye>
-                            </div>
-                        </div>
-                        <div className='mt-4.5 relative w-full'>
-                            <input type="password" className='py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]' placeholder='Confirm Password' />
-                            <div className='absolute left-2 top-5.5 '>
-                                <FaLock className='text-white text-xl '></FaLock>
-                            </div>
-                            <div className='absolute right-5 top-6 cursor-pointer '>
-                                <FaEye className='text-white text-xl '></FaEye>
-                            </div>
-                        </div>
-                        <button class="w-full py-3 rounded-[10px] font-semibold cursor-pointer mt-10 bg-[#C5BB95] text-[#2D2D2D]  border-[#e5e5e5]">
-                            Sign Up
-                        </button>
-                        <p className='text-sm text-[#999999] mt-10'>Privacy Policy and Terms of Service</p>
-                    </div>
-                </div>
-    );
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="h-screen inter relative">
+      <div className="absolute top-1/2 w-[90%] md:w-auto left-1/2 -translate-x-1/2 -translate-y-1/2  px-2 md:px-10 py-8 md:py-14 border border-primary1 rounded-[10px] flex flex-col items-center justify-center">
+        <h1 className="inter text-4xl font-semibold text-[#FAFAFD]">Sign Up</h1>
+        <p className="text-base text-[#FAFAFD] mt-3">
+          You have an already account?{" "}
+          <Link
+            to="/login"
+            className="text-sm text-[#999999] underline cursor-pointer"
+          >
+            Sign in
+          </Link>
+        </p>
+        <button class="lg:px-[160px] py-3 rounded-[10px] font-semibold cursor-pointer mt-4 md:mt-10 bg-[#C5BB95] justify-center text-[#2D2D2D] flex items-center gap-2 w-full border-[#e5e5e5]">
+          <FcGoogle></FcGoogle>
+          <span>Login with Google</span>
+        </button>
+        <div className="flex items-center gap-3 mt-4 md:mt-8">
+          <div className="h-[1px] w-25 md:w-[210px] bg-[#C5BB95]"></div>
+          <span className="text-[#FAFAFD]">or</span>
+          <div className="h-[1px] w-25 md:w-[210px] bg-[#C5BB95]"></div>
+        </div>
+        <div className="md:mt-10 mt-4 relative w-full">
+          <input
+            type="text"
+            {...register("email", {required: true})}
+            className="py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]"
+            placeholder="Email"
+          />
+          {
+            errors.email && <p className="mt-1 text-red-500 text-sm">Email is required</p>
+          }
+          <div className="absolute left-2 top-5 ">
+            <MdOutlineMail className="text-white text-2xl "></MdOutlineMail>
+          </div>
+        </div>
+        <div className="md:mt-10 mt-4 relative w-full">
+          <input
+          {...register("password", {required: true})}
+            type={showPassword ? "text" : "password"}
+            className="py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]"
+            placeholder="Password"
+          />
+          {
+            errors.password && <p className="mt-1 text-red-500 text-sm">Password is required</p>
+          }
+          <div className="absolute left-2 top-5.5 ">
+            <FaLock className="text-white text-xl "></FaLock>
+          </div>
+          <div
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-5 top-6 cursor-pointer "
+          >
+            {showPassword ? (
+              <FaEye className="text-white text-xl" />
+            ) : (
+              <FaEyeSlash className="text-white text-xl" />
+            )}
+          </div>
+        </div>
+        <div className="mt-4.5 relative w-full">
+          <input
+          {...register("confirmPassword", {required: true})}
+            type={showConfirmPassword ? "text" : "password"}
+            className="py-5 text-white px-10 outline-none border rounded-[10px] w-full border-[#C5BB95] placeholder:text-[#FAFAFD]"
+            placeholder="Confirm Password"
+          />
+          {
+            errors.confirmPassword && <p className="mt-1 text-red-500 text-sm">Confirm Password is required</p>
+          }
+          <div className="absolute left-2 top-5.5 ">
+            <FaLock className="text-white text-xl "></FaLock>
+          </div>
+          <div
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            className="absolute right-5 top-6 cursor-pointer "
+          >
+            {showConfirmPassword ? (
+              <FaEye className="text-white text-xl" />
+            ) : (
+              <FaEyeSlash className="text-white text-xl" />
+            )}
+          </div>
+        </div>
+        <button onClick={handleSubmit(onSubmit)} class="w-full py-3 rounded-[10px] font-semibold cursor-pointer mt-4 md:mt-10 bg-[#C5BB95] text-[#2D2D2D]  border-[#e5e5e5]">
+          Sign Up
+        </button>
+        <p className="text-sm text-[#999999] mt-4 md:mt-10">
+          Privacy Policy and Terms of Service
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
